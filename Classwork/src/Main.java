488d97addaae6edc7ad95d9fa6979b6a11ec9b80
Георги Георгиев Котаков <GGKotakov20@codingburgas.bg>
2023-10-10 09:51:03 +0300

    Add tasks

--- Classwork/src/Main.java
+++ Classwork/src/Main.java
@@ -10,18 +10,6 @@ public class Main {
             System.out.println(i+1);
         }
 
-        System.out.println("Third task");
-        for (int i=0;i<5;i++){
-            System.out.print(i+1+" ");
-        }
-
-        System.out.println();
-        System.out.println("Last task");
-        for (int i=0;i<3;i++){
-            for (int j=0;j<5;j++){
-                System.out.print(j+1+" ");
-            }
-            System.out.println();
-        }
+        Second.main();
     }
 }
