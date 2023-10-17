2ab77adad391d6c9c8039dd9a7103e788969ac89
ggkotakov20 <ggkotakov20@coddingburgas.bg>
2023-10-17 10:07:14 +0300

    Add Car package

--- Classwork/src/Main.java
+++ Classwork/src/Main.java
@@ -1,15 +1,7 @@
+import car.MainCar;
+
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("First task");
-        System.out.println("Hello Java!");
-        System.out.println("This is another line");
-
-        System.out.println("Second task");
-        for (int i=0;i<5;i++){
-            System.out.println(i+1);
-        }
-
-        Second.main();
+        MainCar.main();
     }
 }
---
+++ Classwork/src/car/MainCar.java
@@ -0,0 +1,22 @@
+package car;
+
+public class MainCar {
+    public static void main() {
+        Car bmw =  new Car();
+        bmw.setBrand("BMW");
+        bmw.setModel("M6");
+        System.out.println(bmw);
+
+        Car bmw2 = new Car();
+        System.out.println(bmw.hashCode());
+        System.out.println(bmw2.hashCode());
+
+        System.out.println(bmw2);
+
+//        Car mercedes =  new Car();
+//        mercedes.setBrand("Mercedes");
+//        mercedes.setModel("AMG");
+//        System.out.println(mercedes.getBrand());
+//        System.out.println(mercedes.getModel());
+    }
+}
