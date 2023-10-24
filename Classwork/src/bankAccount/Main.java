2e1c9b148cd47b4950711215c53ecddc5aa0432f
ggkotakov20 <ggkotakov20@coddingburgas.bg>
2023-10-24 09:58:59 +0300

    Add bankAccount package

---
+++ Classwork/src/bankAccount/Main.java
@@ -0,0 +1,30 @@
+package bankAccount;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main() {
+        Scanner consoleInput = new Scanner(System.in);
+        String command = consoleInput.nextLine();
+
+        while (!command.equals("End")){
+            String[] tokens = command.split("\\s+");
+            String actualCommand = tokens[0];
+            BankAccount bankAccount;
+
+            if(actualCommand.equals("Create")){
+                bankAccount = new BankAccount();
+            } else if (actualCommand.equals("SetInterest")) {
+                int id= Integer.parseInt(tokens[1]);
+                int ammount= Integer.parseInt(tokens[2]);
+                bankAccount.deposit(ammount);
+            } else if (actualCommand.equals("GetInterest")) {
+
+            } else {
+                System.out.println("Invalid command");
+            }
+
+            command = consoleInput.nextLine();
+        }
+    }
+}
