4f6cde94dafbb19d9d14250b2f62467125cbdb85
ggkotakov20 <ggkotakov20@coddingburgas.bg>
2023-11-14 10:37:27 +0200

    Add FourteenthNovember package

---
+++ Classwork/src/FourteenthNovember/Student.java
@@ -0,0 +1,11 @@
+package FourteenthNovember;
+
+public class Student extends Human{
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public void test(){
+        super.name = "Petio";
+    }
+}
